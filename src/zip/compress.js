0f6edc225e252c4dc058b34ca9511837fd7c4a6d
Irakli Pipia <4u3v2w1@gmail.com>
2025-04-29 01:46:34 +0400

    feat: implement Compression tasks (compress and decompress with gzip)

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import path from 'path';
+import { pipeline } from 'stream/promises';
+
 const compress = async () => {
-    // Write your code here 
+  const sourcePath = path.join('zip', 'files', 'fileToCompress.txt');
+  const destinationPath = path.join('zip', 'files', 'archive.gz');
+
+  const readStream = createReadStream(sourcePath);
+  const gzipStream = createGzip();
+  const writeStream = createWriteStream(destinationPath);
+
+  await pipeline(readStream, gzipStream, writeStream);
 };
 
 await compress();
