5ea3dc905d7fdc2637b3551878c47bbc977172c3
Irakli Pipia <4u3v2w1@gmail.com>
2025-04-29 02:10:08 +0400

    feat: implement Child Process task (spawn child, handle stdin and stdout)

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { spawn } from 'child_process';
+import path from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = path.join('cp', 'files', 'script.js');
+
+  const child = spawn('node', [scriptPath, ...args], {
+    stdio: ['pipe', 'pipe', 'inherit']
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(["arg1", "arg2"]); // this line is just for example, you may remove it for clean final version
